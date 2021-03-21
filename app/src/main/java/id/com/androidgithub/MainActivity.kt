117d5f668d73ea9a556d76c622797798f68c9c1d
M Aqiel Hilman M <maqiel2600@gmail.com>
2021-03-21 20:49:12 +0700

    something

--- app/src/main/java/id/com/androidgithub/MainActivity.kt
+++ app/src/main/java/id/com/androidgithub/MainActivity.kt
@@ -2,10 +2,13 @@ package id.com.androidgithub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Sample", Toast.LENGTH_SHORT).show()
     }
 }
